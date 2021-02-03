c6e7fc619650f90e22b6892294d1dd9c546a4564
sooyeon <20200918@sungshin.ac.kr>
2021-02-04 02:39:14 +0900

    Initial Commit

---
+++ algorithm_study/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  algorithm_study
+//
+//  Created by 수연 on 2021/02/04.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
